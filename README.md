a2efd8e9234307a170d41bee37d16527e06a48ee
ahussan <anjalhussan@Anjal-Hussan-MBP2.local>
2016-08-30 10:32:37 -0400

    arraysum13

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Problem-Solving-in-Java
+
+# Purpose
+
+This repository is created for practice purpose. 
+Practice code in java will be uploaded to this repository.
+ 
---
+++ src/main/java/problemsolving/ArraySum13.java
@@ -0,0 +1,37 @@
+package problemsolving;
+
+/**
+ * Created by anjalhussan on 8/30/16.
+ */
+public class ArraySum13 {
+
+    /*
+    Return the sum of the numbers in the array,
+    returning 0 for an empty array.
+    Except the number 13 is very unlucky,
+    so it does not count and numbers that come
+    immediately after a 13 also do not count.
+    sum13([1, 2, 2, 1]) → 6
+    sum13([1, 1]) → 2
+    sum13([1, 2, 2, 1, 13]) → 6
+    http://codingbat.com/prob/p127384
+     */
+
+    public static void main(String[] args){
+        int[] numbers = new int[]{1,2,3,4,5,13,20,13,20,50};
+        System.out.println(sums(numbers));
+    }
+
+    public static int sums(int[] nums){
+        int result=0;
+        for (int i = 0; i <nums.length ; i++) {
+            if (nums[i]!=13){
+                result=result+nums[i];
+            } else {
+                i=i+1;
+            }
+        }
+        return result;
+    }
+
+}
